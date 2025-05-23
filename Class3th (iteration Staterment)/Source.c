311c90119d7c2d71f378122c45a26fb6bbb1ca23
gydud0819 <pstar0819@naver.com>
2025-05-23 15:42:10 +0900

    (2025-05-23) 반복문

---
+++ Class3th (iteration Staterment)/Source.c
@@ -0,0 +1,135 @@
+/*
+* 작성일	: 2025-05-23
+* 작성자	: 박효영
+* 주제	: 반복문
+*/
+#include <stdio.h>
+
+void main()
+{
+#pragma region 반복문
+	// 프로그램 내에서 특정한 작업을 반복적으로 수행하는 명령문이다.
+#pragma region 증감연산자
+	// 피연산자를 하나씩 증가시키거나 감소시킬 때 사용하는 연산자이다.
+
+#pragma region 전위 증감 연산자
+	// 변수의 값을 증감시킨 후에 연산을 수행하는 연산자이다.
+
+	// int a = 0;
+	// int b = ++a;
+	// 
+	// printf("%d\n", a);
+	// printf("%d\n", b);
+	// 
+	// b = --a;
+	// 
+	// printf("%d\n", a);
+	// printf("%d\n", b);
+#pragma endregion
+
+#pragma region 후위 증감 연산자
+	// 연산을 수행한 다음 변수의 값을 증감시키는 연산자이다.
+	// 
+	// int a = 0;
+	// int b = a++;
+	// 
+	// printf("a 값 : %d\n", a);
+	// printf("b 값 : %d\n", b);
+	// 
+	// b = a--;
+	// printf("a 값 : %d\n", a);
+	// printf("b 값 : %d\n", b);
+
+#pragma endregion
+
+#pragma endregion
+
+#pragma region for문
+	// 초기식을 연산하여 조건식의 결과에 따라 특정한 수만큼 반복하는 반복문이다.
+	// 반복문이 동작하는 순서는 초기화 -> 조건 검사 -> 실행 -> 증감 구조 로 동작한다.
+
+	
+	 for (int i = 1; i < 10; i++)
+	 {
+	 	for (int j = 1; j < 10; j++)
+	 	{
+	 		printf(" %d * %d = %d\n", i, j, i*j);
+	 	}
+	 		printf("===============\n");
+	 }
+
+	/*for (int i = 0; i < 10; i++)
+	{
+		printf("*\n", i);
+		for (int j = 1; j < 10; j++)
+		{
+			printf("*\n", j, i);
+		}
+	}*/
+
+	//printf("==============================\n");
+
+	//// 역 출력
+	//for (int i = 10; i >= 0; i--)
+	//{
+	//	printf("%d\n", i);
+
+	//}
+
+#pragma endregion
+
+#pragma region  while문
+	// 특정 조건을 만족할 때까지 계속해서 주어진 명령문을 실행하는 반복문이다.
+
+	// 반복문을 순차적으로 실행하면서 조건 분기를 만나게 되면 어느쪽으로 실행 흐름이 흐를지 미리 예측한다.
+
+	/*
+	* while (조건)
+	* {
+	*
+	*
+	*
+	*
+	* }
+	*/
+
+#pragma region Do While문
+	// 조건과 상관없이 한번의 작업을 수행한 다음 조건에 따라 명령문을 실행하는 반복문이다.
+	// 반복문의 경우 미리 예측해서 실행하는 구조를 가진다.
+	// 예측이 틀렸다면 현재 반복문 내의 조건 분기만 다시 검사하여 처리한다.
+
+	// int x = 23;
+	// 
+	//  do
+	//  {
+	// 	 printf("★\n"); // ? 
+	// 	 x++;
+	// 
+	//  } while (x < 5);
+	// 
+#pragma endregion
+
+
+#pragma endregion
+
+#pragma region 이중 for문
+// for 문 안에 또 for문이 들어가 있는 것
+//for (int i = 0; i < 3; i++)
+//{
+//	printf("i의 값 : %d\n", i);
+//
+//	for (int j = 0; j < 3; j++) 
+//	{
+//		printf("j의 값 : %d\n", j);
+//	}
+//	printf("===========\n");
+//	
+//}
+
+#pragma endregion
+
+
+
+#pragma endregion
+
+}
--- GameProC/Source.c
+++ GameProC/Source.c
@@ -2,11 +2,73 @@
 
 void main()
 {
-	printf("대입연산자 예시 코드\n");
-	int a = 10;
-	a += 1;
-	printf("대입연산 덧셈 결과값: %d\n", a);
+#pragma region 조건문
+	// 어떤 조건이 주어질 때 해당 조건에 따라 동작을 수행하도록 실행하는 명령문이다.
 
-	a -= 2;
-	printf("대입연산 뺄셈 결과값: %d\n", a);
+#pragma region 관계 연산자
+	// 2개의 피연산자의 값을 비교하여 그 결과를 0 또는 1이라는 값으로 나타내는 연산자이다.
+
+	// > : 왼쪽 값이 더 크다면
+	// < : 오른쪽 값이 더 크다면
+	// >= : 왼쪽 값이 크거나 같다면
+	// <= : 오른쪽 값이 크거나 같다면
+	// == : 값이 서로 다르다면
+	// != : 값이 서로 다르다면 (!는 부정)
+
+	// int s = 1 < 0;
+	// int s1 = 1 > 0;
+	// int s2 = 1 >=0;
+	// int s3 = 1 <= 0;
+	// int s4 = 1 != 0;
+	// int s5 = 1 == 0;
+	// 
+	// printf("%d\n", s);
+	// printf("%d\n", s1);
+	// printf("%d\n", s2);
+	// printf("%d\n", s3);
+	// printf("%d\n", s4);
+	// printf("%d\n", s5);
+
+#pragma endregion
+
+#pragma region if문
+	// 어떤 특정한 조건을 비교하여 조건이 맞다면 실행되는 조건문이다.
+	// if문은 조건이 1일때 실행되고 0일때 실행되지 않는다.
+
+	// int exp = 5;
+	// int maxExp = 5;
+	// if (exp >= maxExp)	// 최대 경험치보다 얻은 경험치가 더 크면 
+	// {
+	// 	printf("1레벨 증가했다.\n"); // ? 
+	// }
+	// else
+	// {
+	// 	printf("경험치가 부족하다.\n");
+	// }
+
+#pragma endregion
+
+#pragma region else if문
+	// 여러개의 조건을 순차적으로 검사하고 싶을 때 사용하는 조건문이다.
+	// else if문의 경우 여러 개를 사용할 수 있고 if문부터 하나씩 검사하다가 가장 먼저 조건이 맞는 분기로 들어간다.
+
+	// int age = 24;
+	// if (age == 30)
+	// 
+	// {
+	// 	printf("누가 30이래;\n");
+	// }
+	// else if (age == 24)
+	// {
+	// 	printf("24살이야 ><\n");
+	// }
+
+#pragma endregion
+
+#pragma region Switch문 (월 예정)
+
+#pragma endregion
+
+#pragma endregion
+
 }
